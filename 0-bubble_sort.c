f10eeda838a811633fb1eade5d792e8ed5e6f965
Hussein Yussuf <huznigarane@gmail.com>
2024-02-16 14:23:09 +0300

    corrected the logic, added betty documentation, added the prototypes in the sort.h file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,36 @@
 #include "sort.h"
+#include <stddef.h>
+/**
+ * bubble_sort - function to that sorts array of integers using bubble sort
+ * @array: the array to be sorted
+ * @size: size  of the array
+ * Return: returns the sorted array
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
-	/*loop through the array*/
+	size_t i, j;
+	int temp;
+
+	/*check if the array is null or its size is less than2*/
+	/*return as the array is aklready sorted*/
+	if (array == NULL || size < 2)
+		return;
+
+	/*loop through the array to second last*/
 	for (i = 0; i < size - 1; i++)
 	{
-		/*inner loop to find the index of the current value*/
+		/*inner loop to compare the adjacent elements and swap*/
 		for (j =0; j < size - i - 1; j++)
 		{
 			/*swap if the current is greater*/
-			if array[j] > array[j + 1]
+			if (array[j] > array[j + 1])
 			{
-				int *temp = *a;
-				*a = *b;
-				*b = *temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
-			return (array[i]);
 		}
 	}
 }
