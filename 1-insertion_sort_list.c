1f809c91e89735077a019c76c737760385cfc78e
Hussein380 <huznigarane@gmail.com>
2024-02-17 11:22:07 +0300

    modified insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,61 +14,41 @@ void insertion_sort_list(listint_t **list)
 	/*Nothing to sort if the list is empty or has only one node*/
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
 	/*start from the second node*/
 	current = (*list)->next;
-
 	while (current != NULL)
 	{
 		/*save the next node before moving the current*/
 		next = current->next;
-
 		/*start looking for insertion point from the end oflist*/
 		insertion_point = current->prev;
-
-		/*Traverse backward and find the insertion point*/
-		while(insertion_point != NULL && insertion_point->n > current->n)
+		while (insertion_point != NULL && insertion_point->n > current->n)
 		{
 			insertion_point = insertion_point->prev;
 		}
-
-		/*if insertion point is NULL current should be*/
-		 /* inserted at the beginning*/
 		if (insertion_point == NULL)
 		{
-			/*update the next pointer of the node current*/
 			current->prev->next = next;
 			if (next != NULL)
 				next->prev = current->prev;
-
-			/*Update pointers  to insert current at the beginin*/
 			current->prev = NULL;
 			current->next = *list;
 			(*list)->prev = current;
 			*list = current;
+			print_list((const listint_t*)*list);
 		}
 		else
 		{
-			/*otherwise insert current at the insertion point*/
 			current->prev->next = next;
 			if (next != NULL)
 				next->prev = current->prev;
-
-			/*update pointers to insert current at the*/
-			/*insertion point*/
 			current->prev = insertion_point;
 			current->next = insertion_point->next;
-
-			/*update the prev pointer of node after */
-			 /* insertion_point*/
 			if (insertion_point->next != NULL)
 				insertion_point->next->prev = current;
-
-			/*update the next pointer of insertion_point*/
 			insertion_point->next = current;
 		}
-		/*Move to the next node in the list*/
 		current = next;
-	
+		print_list((const listint_t*)*list);
 	}
 }
